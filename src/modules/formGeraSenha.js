4eab155c82dc69f1564da845232dd6f6cac265a8
Felipe Rodrigues <feliperodrigues_spfc@hotmail.com>
2025-07-26 16:18:37 -0300

    gerador-senha
    Um pequeno gerador de senha, com a possibilidade de gerar uma senha de até 30 caracteres com as opções que desejar

---
+++ src/modules/formGeraSenha.js
@@ -0,0 +1,26 @@
+import geraSenha from './gerador';
+
+const resultado = document.querySelector('.resultado');
+const qtdCaracteres = document.querySelector('.qtd-caracter');
+const checkMaiusculas = document.querySelector('.check-maiusculas');
+const checkMinusculas = document.querySelector('.check-minusculas');
+const checkNumeros = document.querySelector('.check-numeros');
+const checkSimbolos = document.querySelector('.check-simbolos');
+const btGera = document.querySelector('.gerar-senha');
+
+export default () => {
+    btGera.addEventListener('click', () => {
+        resultado.innerHTML = gera();
+        resultado.classList.add('senha-gerada');
+        
+        if (qtdCaracteres.value <= 0) {
+            resultado.innerHTML = 'Escolha o tamanho da sua senha';
+        }
+    })
+}
+
+function gera() {
+
+    const senha = geraSenha(qtdCaracteres.value, checkMaiusculas.checked, checkMinusculas.checked, checkNumeros.checked, checkSimbolos.checked);
+    return senha || 'Nada selecionado';
+}
